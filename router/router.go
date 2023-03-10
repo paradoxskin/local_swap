938a1c7640ed4a266e05a14d7313d76ad39be8e9
paradoxskin <1312269430@qq.com>
2023-03-10 21:05:26 +0800

    adjust frame

--- router/router.go
+++ router/router.go
@@ -7,6 +7,13 @@ import (
 
 func Build() *gin.Engine {
 	r := gin.Default()
+
+	r.LoadHTMLGlob("templ/*")
+
+	r.Static("/js", "static/js")
+	r.Static("/css", "static/css")
+	r.Static("/files", "static/files")
+
 	r.GET("/", controller.GetPage)
 	r.POST("/", controller.PostFileList)
 	r.POST("/upload", controller.UploadFile)
